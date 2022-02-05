5741054652b039d593e5947034ddf9a1e2357137
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2022-02-05 13:42:43 -0800

    Change form to work with drag and drop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,34 @@
 // import React, { useCallback, useMemo,useState } from 'react';
-// import { useDropzone } from 'react-dropzone';
-// import axios from 'axios';
-// const baseStyle = {
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center',
-//   padding: '20px',
-//   borderWidth: 2,
-//   borderRadius: 2,
-//   borderColor: '#eeeeee',
-//   borderStyle: 'dashed',
-//   backgroundColor: '#fafafa',
-//   color: '#bdbdbd',
-//   transition: 'border .3s ease-in-out',
-// };
-
-// const activeStyle = {
-//   borderColor: '#2196f3'
-// };
-
-// const acceptStyle = {
-//   borderColor: '#00e676'
-// };
-
-// const rejectStyle = {
-//   borderColor: '#ff1744'
-// };
-
-// function App(props) {
-//   const [url, setUrl] =useState();
-
-//   const onDrop = useCallback(acceptedFiles => {
-//     setUrl(acceptedFiles[0].path);
-
-//   }, []);
-//   const handleChange = () =>{
-//     var bodyFormData = new FormData();
-//     bodyFormData.append('file', url)
-//     axios({
-//       method: "post",
-//       url: "http://localhost:5000/api/upload",
-//       data: bodyFormData,
-//       headers: { "Content-Type": "multipart/form-data" },
-//     })
-//       .then(function (response) {
-//         //handle success
-//         console.log(response);
-//       })
-//       .catch(function (response) {
-//         //handle error
-//         console.log(response);
-//       });
-//     }
-
-//   const {
-//     getRootProps,
-//     getInputProps,
-//     isDragActive,
-//     isDragAccept,
-//     isDragReject
-//   } = useDropzone({
-//     onDrop,
-//     accept: 'video/mp4, audio/mp3'
-//   });
-
-//   const style = useMemo(() => ({
-//     ...baseStyle,
-//     ...(isDragActive ? activeStyle : {}),
-//     ...(isDragAccept ? acceptStyle : {}),
-//     ...(isDragReject ? rejectStyle : {})
-//   }), [
-//     isDragActive,
-//     isDragReject,
-//     isDragAccept
-//   ]);
-
-//   return (
-//     <>
-//         <h1> Study Helper</h1>
-
-//     <div {...getRootProps({style})}>
-//       <input {...getInputProps()} />
-//       <div>Drag and drop your videos here.</div>
-//     </div>
-//     <button onClick={handleChange}>Upload</button>
-//     </>
-//   )
-// }
-
-// export default App;
-
-import React from 'react';
+import React, { useMemo } from 'react';
 import axios from 'axios';
 import ReactPlayer from 'react-player';
+import { useDropzone } from 'react-dropzone';
+// import axios from 'axios';
+const baseStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '20px',
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: '#eeeeee',
+  borderStyle: 'dashed',
+  backgroundColor: '#fafafa',
+  color: '#bdbdbd',
+  transition: 'border .3s ease-in-out',
+};
+
+const activeStyle = {
+  borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+  borderColor: '#00e676'
+};
+
+const rejectStyle = {
+  borderColor: '#ff1744'
+};
 
 const App = () => {
   // a local state to store the currently selected file.
@@ -121,18 +57,50 @@ setVideoFilePath(URL.createObjectURL(event.target.files[0]));
   }
 
   const handleFileSelect = (event) => {
+    console.log("handle file")
     setSelectedFile(event.target.files[0])
     setVideoFilePath(URL.createObjectURL(event.target.files[0]));
   }
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({
+    onDrop: (acceptedFiles) => {
+      setSelectedFile(acceptedFiles[0])
+      setVideoFilePath(URL.createObjectURL(acceptedFiles[0]));
+    },
+    accept: 'video/mp4, audio/mp3'
+  });
+
+  const style = useMemo(() => ({
+    ...baseStyle,
+    ...(isDragActive ? activeStyle : {}),
+    ...(isDragAccept ? acceptStyle : {}),
+    ...(isDragReject ? rejectStyle : {})
+  }), [
+    isDragActive,
+    isDragReject,
+    isDragAccept
+  ]);
+
+
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileSelect}/>
+      {/* <input type="file" onChange={handleFileSelect}/> */}
+      <div {...getRootProps({style})}>
+        <input {...getInputProps()} />
+        <div>Drag and drop your videos here.</div>
+      </div>
       <input type="submit" value="Upload File" />
       <ReactPlayer url = { videoFilePath }  width="100%" height="100%" controls={true} />
-"
     </form>
   )
 };
+
+
 
 export default App;
