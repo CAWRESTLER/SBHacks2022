e444ff8af9b6371dc904443260d5c54bb9696878
CAWRESTLER <mx9556@gmail.com>
2022-02-05 23:10:10 -0800

    Buttons seek in video

--- frontend/src/Player/index.js
+++ frontend/src/Player/index.js
@@ -1,31 +1,26 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import ReactPlayer from 'react-player';
 const data = require("./dummy2.json")
 
 
-function Chapter ({ chapter }) {
+
+function Player ({ videoPath })  {
+  const player = useRef();
+  const handleClick = (event) => {
+    console.log(event.target.innerText)
+    let seekTimeArr = event.target.innerText.split(":")
+    let seekTime = Number(seekTimeArr[0]*60) + Number(seekTimeArr[1])
+    player.current.seekTo(seekTime)
+  }
   function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
   }
-  const handleClick = (event) => {
-    console.log(event.target.innerText)
-  }
-  console.log(chapter)
+  
   return (
     <div>
-        <div> <b>Gist: </b> {chapter.gist} </div>
-      <div> <b>Timestamp: </b> <button onClick={handleClick}>{millisToMinutesAndSeconds(chapter.start)} </button> -- <button onClick={handleClick}>{millisToMinutesAndSeconds(chapter.end)}</button> </div>
-    </div>
-  )
-}
-
-function Player ({ videoPath })  {
-
-  return (
-    <div>
-        <ReactPlayer  url={"https://www.youtube.com/watch?v=h7apO7q16V0"} width="100%" height="100%" controls={true} />
+        <ReactPlayer  ref={player}url={"https://www.youtube.com/watch?v=h7apO7q16V0"} width="100%" height="100%" controls={true} />
     <h1>Video transcript:  </h1>
       <p>
         {data.text}
@@ -33,7 +28,14 @@ function Player ({ videoPath })  {
       <h1>Timestamps</h1>
       <div>
         {
-          data.chapters.map(chapter => <Chapter chapter={chapter}/> )
+          data.chapters.map(chapter => {
+            return(
+              <div>
+                <div> <b>Gist: </b> {chapter.gist} </div>
+                <div> <b>Timestamp: </b> <button onClick={handleClick}>{millisToMinutesAndSeconds(chapter.start)} </button> -- <button onClick={handleClick}>{millisToMinutesAndSeconds(chapter.end)}</button> </div>
+              </div>
+            );
+          } )
         }
       </div>
 
